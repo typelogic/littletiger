7c40b21f0c692fc006f4be697e6853019c9aeabc
Dexter Aparicio <dexter@newlogic.io>
2020-08-25 18:27:02 +0800

    First successful POST response using postman

---
+++ src/main/java/com/example/littletiger/service/PersonService.java
@@ -0,0 +1,22 @@
+package com.example.littletiger.service;
+
+import com.example.littletiger.dao.PersonDao;
+import com.example.littletiger.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+}
