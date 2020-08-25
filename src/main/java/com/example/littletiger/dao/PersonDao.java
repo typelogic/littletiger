ce65501a3420577a9b876949eeca3551597b2fbf
Dexter Aparicio <dexter@newlogic.io>
2020-08-25 18:36:33 +0800

    Implement http GET response

--- src/main/java/com/example/littletiger/dao/PersonDao.java
+++ src/main/java/com/example/littletiger/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.example.littletiger.dao;
 
 import com.example.littletiger.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -12,4 +13,6 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
 }
