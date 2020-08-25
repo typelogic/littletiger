2d9e71776cd63b988c1592b3344d80877a576890
Dexter Aparicio <dexter@newlogic.io>
2020-08-25 19:37:55 +0800

    Simplest example of DI in action

---
+++ src/main/java/com/example/littletiger/dao/PersonDataAccessService.java
@@ -0,0 +1,39 @@
+package com.example.littletiger.dao;
+
+import com.example.littletiger.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository("posgres")
+public class PersonDataAccessService implements PersonDao {
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return Stream.of(new Person(UUID.randomUUID(), "Pos Gres"))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
